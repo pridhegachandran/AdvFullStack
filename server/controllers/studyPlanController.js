cbb3904ad41beb381c8c4051588e2f1e9dbdf227
pridhegac <pridhegac.23csd@kongu.edu>
2025-12-12 11:56:20 +0530

    updated

--- server/controllers/studyPlanController.js
+++ server/controllers/studyPlanController.js
@@ -2,7 +2,7 @@ import studyPlan from '../models/StudyPlan.js';
 
 
 export const createStudyPlan = async (req, res) => {
-    const { planName, startDate, endDate, description, specialConstrains, aiSuggestions } = req.body;
+    const { planName, startDate, endDate, description, specialConstraints, aiSuggestion} = req.body;
 
 
     if (!planName || !startDate || !endDate || !description) {
@@ -16,8 +16,8 @@ export const createStudyPlan = async (req, res) => {
             startDate,
             endDate,
             description,
-            specialConstrains,
-            aiSuggestions
+            specialConstraints,
+            aiSuggestion
         });
         const savedPlan = await newStudyPlan.save();
         return res.status(201).json({message: 'plan created successfully'}, savedPlan);
@@ -37,7 +37,7 @@ export const getStudyPlans = async (req, res) => {
 }
 export const editStudyPlan = async (req, res) => {
     const { id } = req.params;
-    const { planName, startDate, endDate, description, specialConstrains, aiSuggestions } = req.body;
+    const { planName, startDate, endDate, description, specialConstraints, aiSuggestion } = req.body;
 
 
     const ifexist = await studyPlan.findById(id);
@@ -52,8 +52,8 @@ export const editStudyPlan = async (req, res) => {
             startDate,
             endDate,
             description,
-            specialConstrains,
-            aiSuggestions
+            specialConstraints,
+            aiSuggestion
         }, { new: true });
 
 
